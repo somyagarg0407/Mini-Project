0983f329aabac854886ff4b0b16ebaaf6e5cfb43
somyagarg0407 <somyagarg0407@gmail.com>
2026-01-18 15:43:55 +0530

    Update calculator.c

--- Calculator (backend only)/calculator.c
+++ Calculator (backend only)/calculator.c
@@ -1,49 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
 
     float num[30];
     char opr[30];
 
     printf("Now enter the Numbera and operators to be calculated \n ");
-    for(int i=0;i<30;i++){
-        scanf("%f",&num[i]);
-        scanf(" %c",&opr[i]);
-        if(opr[i]=='='){
-                break;
-            }
-    }
-    
-    float result = num[0];
-    
-    for(int i=0;i<30;i++){
-        
-        switch(opr[i]){
-            case '+':
-            result=result + num[i+1];
+    for (int i = 0; i < 30; i++)
+    {
+        scanf("%f", &num[i]);
+        scanf(" %c", &opr[i]);
+        if (opr[i] == '=')
+        {
             break;
-
-            case '-':
-            result=result - num[i+1];
-            break;
-
-            case '*':
-            result=result * num[i+1];
-            break;
-
-            case '/':
-            result=result / num[i+1];
-            break;
-
-            case '=':
-            break;
-
-            default:
-            printf("\n");
-
         }
     }
-    printf("The final result is: %.2f",result);
+
+    float result = num[0];
+
+    for (int i = 0; i < 30; i++)
+    {
+
+        switch (opr[i])
+        {
+        case '+':
+            result = result + num[i + 1];
+            break;
+
+        case '-':
+            result = result - num[i + 1];
+            break;
+
+        case '*':
+            result = result * num[i + 1];
+            break;
+
+        case '/':
+            result = result / num[i + 1];
+            break;
+
+        case '=':
+            goto end; // --------------------------------EXIT loop completely---------------------------------
+        }
+    }
+
+end: //--------------------------------------------------------------IMP---------------------------------------------
+    printf("The final result is: %.2f", result);
 
     return 0;
 }
